38f05f290ca9581b1aae0ca553eb0a188b5f83d9
zhaoye <zy81218@gmail.com>
2016-12-01 20:09:52 +0800

    Add: record media url

--- mockserver/blueprints/media/media.py
+++ mockserver/blueprints/media/media.py
@@ -1,5 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from mockserver import recorder
 
 media = Blueprint('media', __name__,
                   url_prefix='/media',
                   static_folder='static')
+
+
+@media.before_request
+def before_request():
+    recorder.add(request.url)
